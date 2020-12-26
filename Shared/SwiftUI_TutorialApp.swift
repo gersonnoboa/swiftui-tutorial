86899f47cd3a0f4ccecc26e880cb562eed1caf51
Gerson Noboa <gerson.noboa@glia.com>
2020-12-26 20:12:24 +0200

    Initial Commit

---
+++ Shared/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  Shared
+//
+//  Created by Gerson Noboa on 26/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
