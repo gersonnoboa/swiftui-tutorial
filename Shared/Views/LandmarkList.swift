20880e1d761c1f53b3b675dfac50668af5a10884
Gerson Noboa <gerson.noboa@glia.com>
2021-01-22 08:20:07 +0200

    Full list and navigation tutorial

---
+++ Shared/Views/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Gerson Noboa on 20/1/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+    }
+}
